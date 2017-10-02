50f3f3a5d5a9690e95ee6ad7ca82c3ce655f62e1
J3FALL <crisp.coad@gmail.com>
2017-10-02 23:38:16 +0300

    Add test for System.nanoTime to seconds convertation

---
+++ src/test/java/TimeTest.java
@@ -0,0 +1,21 @@
+import lombok.SneakyThrows;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TimeTest {
+
+    @Test
+    @SneakyThrows
+    public void testTransformNanoTimeToSeconds() {
+
+        Long firstMoment = System.nanoTime();
+        Thread.sleep(500);
+        Long secondMoment = System.nanoTime();
+
+        assertThat(Integer.valueOf((int) (secondMoment - firstMoment / 1000)))
+                .isInstanceOf(Integer.class);
+
+        System.out.println();
+    }
+}
